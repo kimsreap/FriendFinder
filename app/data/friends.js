04d9d0d48a66777ec7f8c30fd2e6e1223074131e
Kimsreang Phan <kimsreangphan@Kimsreangs-MacBook-Pro.local>
2019-05-19 15:36:29 -0700

    added similar format on friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+const FriednList = [
+    {
+        "name":"WonderWoman",
+        "photo":"https://www.google.com/search?q=wonder+woman&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiVquHE0KjiAhXIqp4KHSdFBGwQ_AUIDigB&biw=1298&bih=714#imgrc=pIAP9mXeY92sdM:",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    },
+    {
+        "name":"Batman",
+        "photo":"https://www.google.com/search?q=wonder+woman&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiVquHE0KjiAhXIqp4KHSdFBGwQ_AUIDigB&biw=1298&bih=714#imgrc=pIAP9mXeY92sdM:",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    },
+    {
+        "name":"Superman",
+        "photo":"https://www.google.com/search?q=wonder+woman&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiVquHE0KjiAhXIqp4KHSdFBGwQ_AUIDigB&biw=1298&bih=714#imgrc=pIAP9mXeY92sdM:",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    }
+    
+];
+module.exports = FriendList
+
+
+  
