d83cdb6a6eef6528d23892180e55e34804568689
Kimsreang Phan <kimsreangphan@Kimsreangs-MacBook-Pro.local>
2019-05-21 18:16:32 -0700

    tried to work on route

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,9 @@
+// Get routes to /survey
+// catch all route that leads to home.html 
+
+// how do i set up the route to display the raw Json Data?
+
+//  how to create the for loop to make the logic work?
+
+// how do i send the response back to the survey page on survey.html?
+// 
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var path = require("path");
+
+module.exports = function(app){
+    app.get(req,res) {
+        // not sure what to put in here
+
+    }
+}
